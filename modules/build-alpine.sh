9d13df1c07d31efda9d9b82c54ed9fe5830990df
Michael Clausen <encodeering@gmail.com>
2018-01-20 14:30:18 +0100

    adds a project definition for nginx 1.12
    * stable directory contains 1.12; should be made configurable by bash script if we want to support mainline as well
    
    NOTE
    
    * armhf needs to be build on semaphore due timeouts for debian; alpine seems to work

---
+++ modules/build-alpine.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -e
+
+import com.encodeering.ci.config
+import com.encodeering.ci.docker
+
+docker-pull "$REPOSITORY/alpine-$ARCH:3.7" "alpine:3.7"
+
+docker-build "$PROJECT/stable/alpine"
---
+++ modules/build-debian.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -e
+
+import com.encodeering.ci.config
+import com.encodeering.ci.docker
+
+docker-pull "$REPOSITORY/debian-$ARCH:stretch" "debian:stretch-slim"
+
+docker-build "$PROJECT/stable/stretch"
