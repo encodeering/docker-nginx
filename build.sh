57997212caadc441ea216748e1a06aedfe0c8f38
Michael Clausen <encodeering@gmail.com>
2018-10-27 12:02:14 +0200

    updates nginx image to 1.14
    * latest release

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ set -e
 import com.encodeering.ci.config
 import com.encodeering.ci.docker
 
-docker-pull "$REPOSITORY/nginx-$ARCH:1.12-alpine" "nginx:alpine"
+docker-pull "$REPOSITORY/nginx-$ARCH:1.14-alpine" "nginx:alpine"
 
 docker-build alpine
 
