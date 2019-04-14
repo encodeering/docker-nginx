2dfb8d8135f0196f5358caeaf09fe800ed8299b3
Michael Clausen <encodeering@gmail.com>
2019-04-14 12:09:27 +0200

    integrates toggles for redirection and customization trait
    * last listen directive will be copied, which means care must be taken for vhost definition (2 listen directives for last block)
    
    NOTE
    
    * busybox compliance is necessary, e.g. grep

--- modules/sequel/bin/docker-entrypoint.sh
+++ modules/sequel/bin/docker-entrypoint.sh
@@ -3,12 +3,43 @@
 set -e
 set -o pipefail
 
-mkdir -p             nginx
-chown nginx:www-data nginx
-chmod 750            nginx
-
 NGINX_VHOST="${NGINX_CONFDIR}/conf.d/vhost.conf"
 
-cat "${NGINX_CONFDIR}/conf-available.d/${VHOST}.conf" > "${NGINX_VHOST}"
+die () {
+    echo "$1"
+    exit 1
+}
+
+base () {
+    local   file="${NGINX_CONFDIR}/conf-available.d/${VHOST}.conf"
+
+    [ -f "${file}" ] || die "vhost ${VHOST} not available"
+
+    cat  "${file}" > "${NGINX_VHOST}"
+}
+
+canonical () {
+    local listen=`cat "${NGINX_VHOST}" | grep -oE '^.*listen.*$' | tail -n2 | tr '\n' '@'`
+    local file="${NGINX_CONFDIR}/conf-available.d/trait/${VHOST_CANONICAL}.conf"
+
+    [ -z "${VHOST_CANONICAL}" ] && return
+    [ -f "${file}" ] || die "canonical trait ${VHOST_CANONICAL} not available"
+
+    cat  "${file}" | sed -e "s/^.*{{LISTEN}}$/${listen}/g;s/@/\n/g" >> "${NGINX_VHOST}"
+}
+
+customization () {
+    [ "${VHOST_CUSTOMIZATION}" = "true" ] && {
+        mkdir -p             nginx;
+        chown nginx:www-data nginx || die "chown couldn't be applied";
+        chmod 750            nginx || die "chmod couldn't be applied";
+    } || {
+        sed -i -e '/customization.conf;$/d' "${NGINX_VHOST}";
+    }
+}
+
+base
+canonical
+customization
 
 exec nginx "$@"
